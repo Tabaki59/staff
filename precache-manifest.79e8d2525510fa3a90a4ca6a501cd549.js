718b3bae51f0adb3407365c9330d758e9c579f0e
Alexandr <alexber59rus@gmail.com>
2021-09-22 10:18:13 +0500

    Updates

---
+++ precache-manifest.79e8d2525510fa3a90a4ca6a501cd549.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c606543e4c8bf6002737de50e480c1f",
+    "url": "/staff/index.html"
+  },
+  {
+    "revision": "b802e8dabd8a78887123",
+    "url": "/staff/static/css/2.7f2699be.chunk.css"
+  },
+  {
+    "revision": "fc3fa315c5f66baed901",
+    "url": "/staff/static/css/main.f4ef9a05.chunk.css"
+  },
+  {
+    "revision": "b802e8dabd8a78887123",
+    "url": "/staff/static/js/2.a7bed360.chunk.js"
+  },
+  {
+    "revision": "0998ed12bc68cac1030b8438636dce91",
+    "url": "/staff/static/js/2.a7bed360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc3fa315c5f66baed901",
+    "url": "/staff/static/js/main.deb4805a.chunk.js"
+  },
+  {
+    "revision": "d002c33f608a2da4547d",
+    "url": "/staff/static/js/runtime-main.b654a80a.js"
+  }
+]);
